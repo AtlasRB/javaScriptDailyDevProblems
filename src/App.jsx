1d54e0f69d4277755768df77ef4cef4274b4a2a0
AtlasRB <connoryeeles@outlook.com>
2024-05-08 12:36:50 +0100

    Made carousel work as intended with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Carousel from "./Components/carousel/index"
+import Carousel12 from "./Components/carousel/carousel12";
+import Carousel6 from "./Components/carousel/carousel6";
 import Test from "./Components/pages/testPage";
 
 function App() {
@@ -6,10 +7,21 @@ function App() {
     { text: 'Page A', url: '/hello', backGround: 'bg-blue-400'},
     { text: 'Page B', url: '/12', backGround: 'bg-red-400'},
     { text: 'Page C', url: '/32', backGround: 'bg-green-400'},
-    { text: 'Page A', url: '/43', backGround: 'bg-blue-400'},
+    { text: 'Page X', url: '/23', backGround: 'bg-blue-400'},
+    { text: 'Page Y', url: '/21', backGround: 'bg-red-400'},
+    { text: 'Page Z', url: '/423', backGround: 'bg-green-400'},
+    { text: 'Page 1', url: '/hello', backGround: 'bg-blue-400'},
+    { text: 'Page 2', url: '/12', backGround: 'bg-red-400'},
+    { text: 'Page 3', url: '/32', backGround: 'bg-green-400'},
+    { text: 'Page 4', url: '/23', backGround: 'bg-blue-400'},
+    { text: 'Page 5', url: '/21', backGround: 'bg-red-400'},
+    { text: 'Page 6', url: '/423', backGround: 'bg-green-400'}
   ];
 
   const pages2 = [
+    { text: 'Page A', url: '/hello', backGround: 'bg-blue-400'},
+    { text: 'Page B', url: '/12', backGround: 'bg-red-400'},
+    { text: 'Page C', url: '/32', backGround: 'bg-green-400'},
     { text: 'Page X', url: '/23', backGround: 'bg-blue-400'},
     { text: 'Page Y', url: '/21', backGround: 'bg-red-400'},
     { text: 'Page Z', url: '/423', backGround: 'bg-green-400'}
@@ -20,13 +32,13 @@ function App() {
       <h1 className="mb-16 flex justify-center">Test</h1>
       
       <h2 className="mt-16 flex justify-center">Beginner</h2>
-      <Carousel data={pages1}></Carousel>
+      <Carousel12 data={pages1}></Carousel12>
 
       <h2 className="mt-16 flex justify-center">Intermediate</h2>
-      <Carousel data={pages2}></Carousel>
+      <Carousel6 data={pages2}></Carousel6>
 
       <h2 className="mt-16 flex justify-center">Advanced</h2>
-      <Carousel data={pages2}></Carousel>
+      <Carousel6 data={pages2}></Carousel6>
 
       <a href={<Test />}>Testing</a>
 
