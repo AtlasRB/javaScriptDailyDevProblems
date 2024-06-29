7b87ab8fa481b39b538e38a1ba51dce47eeb5714
AtlasRB <connoryeeles@outlook.com>
2024-06-29 09:59:29 +0100

    Improved sizing for not attempted page and not found page

--- src/Components/pages/notAttempted.jsx
+++ src/Components/pages/notAttempted.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function NotAttempted() {
   
   return (
-    <div className='font-mono text-5xl text-center'>
+    <div className='font-mono lg:text-5xl text-2xl text-center mx-[5%]'>
       <h1 className='mb-16'>Code Problem not yet attempted</h1>
       <h2 className='mb-16'>Check back later to see my solutions!</h2>
       <Link to='/' className='text-blue-300 hover:underline'>Link back to home page</Link>
--- src/Components/pages/notFound.jsx
+++ src/Components/pages/notFound.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function NotFound() {
   
   return (
-    <div className='font-mono text-5xl text-center'>
+    <div className='font-mono lg:text-5xl text-2xl text-center mx-[5%]'>
       <h1 className='mb-16'>Page not found</h1>
       <Link to='/' className='text-blue-300 hover:underline'>Link back to home page</Link>
     </div>
